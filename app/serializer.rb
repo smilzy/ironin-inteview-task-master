c5f28175112812cf26faf289da1884cd2800fda9
Aleksander Woźniak <4wozniak@gmail.com>
2021-09-17 15:03:25 +0200

    Add Serializer.rb logic

--- app/serializer.rb
+++ app/serializer.rb
@@ -1,2 +1,23 @@
 class Serializer
+  attr_reader :object
+
+  def initialize(object)
+    @object = object
+  end
+
+  class << self
+    def attributes
+      @attributes ||= {}
+    end
+
+    def attribute(name, &block)
+      attributes[name] = block || proc { object.public_send(name) }
+    end
+  end
+
+  def serialize
+    self.class.attributes.transform_values do |block|
+      instance_eval(&block)
+    end
+  end
 end
